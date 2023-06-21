aca0e65b73c257e607eecfc8b1f8afca9db0e7cb
Vimal-Pandey <136955719+Vimal-Pandey@users.noreply.github.com>
2023-06-21 12:30:43 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -33,6 +33,7 @@ function MyApp({ Component, pageProps }) {
           name="viewport" 
           content="initial-scale=1.0, width=device-width" 
         />
+        <meta name="keywords" content="webdesign,website,web design,kanpur,mobile,ios,android,startup,india,noida,vector microstock,design,logo,illustrations,application,business,card,business card,web development,web design india,web development india,web design usa,web design kanpur,web development kanpur,web design company,web development in the uk,illustration design,stock images,agency,web design in the uk,e-commerce,specialists uk,corporate internet services india,internet consultant india,content,management india,designers india,application development leeds,database development,design services,usability,accessibility,web development leeds,india web site design,web solutions,website design,website solutions,leeds website development,india web,website build,ria,flex,flash,actionscript,actionscript3,as2,as3,ajax,html,dhtml,css,xhtml,mysql,sql java,asp,.net,server,consultants,web agency,web development agency,html5,css3,iphone,blackberry,symfony,joomla,php,crm,cms,wordpress,ex-yahoo,adobe,rich internet application,web application,flash animation,adobe flash,adobe flex,air,adobe air,microformats,semantic,markup,ebook,publishing,print,media,ffmpeg,video,transcoding,technologist">
       </Head>
 
       <Component {...pageProps} />
