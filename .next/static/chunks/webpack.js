bb9801453581f451bf792f23f326bf5a8411521c
Satvickk <lenovo37819@gmail.com>
2024-01-01 13:24:13 +0530

    initial commit

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "35fdeb8d1ff9f1df"; }
+/******/ 		__webpack_require__.h = function() { return "5133a6588dfc8571"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
